5e4a8edc355c238e1fd995cc26370c2c6b6ad436
Shreyas Agnihotri <shreyas.v.agnihotri@gmail.com>
2019-03-14 20:28:02 -0400

    Created gradient landing screen with animated clouds

--- viewPoint/ViewController.swift
+++ viewPoint/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import Foundation
+
+let WHITE = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0)
+let TRANSPARENT_WHITE = UIColor(red:0.96, green:0.96, blue:0.96, alpha:0.2)
+let GRAY = UIColor(red:0.25, green:0.32, blue:0.31, alpha:1.0)
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var cloudsView: UIImageView!
+    @IBOutlet weak var cloudsViewLeading: NSLayoutConstraint!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        logInButton.backgroundColor = TRANSPARENT_WHITE
+//        logInButton.layer.cornerRadius = 20
+//        logInButton.layer.borderWidth = 2
+//        logInButton.layer.borderColor = WHITE.cgColor
+        
+
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.cloudsAnimation()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.cloudsViewLeading.constant = 0
+        self.cloudsView.layer.removeAllAnimations()
+        self.view.layoutIfNeeded()
+    }
+
+
+    func cloudsAnimation() {
+        let distance = self.view.bounds.width - self.cloudsView.bounds.width
+        self.cloudsViewLeading.constant = distance
+        UIView.animate(withDuration: 5, delay: 0, options: [.repeat, .curveLinear], animations: {
+            self.view.layoutIfNeeded()
+        })
     }
 
 
 }
+
+
 
