eec6018d35ec50c8e482074283ca9db6e453f13e
Shreyas Agnihotri <shreyas.v.agnihotri@gmail.com>
2019-03-16 00:39:40 -0400

    Implemented signing in and signing out between two screens

---
+++ ViewPoint/DashboardViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DashboardViewController.swift
+//  ViewPoint
+//
+//  Created by Shreyas Agnihotri on 3/15/19.
+//  Copyright © 2019 Shreyas Agnihotri. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class DashboardViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func signOutPressed(_ sender: Any) {
+        
+        print("\nAttempting to sign out\n")
+        
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+            return
+        }
+        
+        GIDSignIn.sharedInstance()?.signOut()
+        
+        print("\nSigned out\n")
+        performSegue(withIdentifier: "goToLogIn", sender: self)
+        
+    }
+    
+    
+    
+}
+
+
--- ViewPoint/LogInViewController.swift
+++ ViewPoint/LogInViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  viewPoint
+//  ViewPoint
 //
 //  Created by Shreyas Agnihotri on 3/14/19.
 //  Copyright © 2019 Shreyas Agnihotri. All rights reserved.
@@ -18,15 +18,15 @@ let GRAY = UIColor(red:0.25, green:0.32, blue:0.31, alpha:1.0)
 class LogInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
 
     @IBOutlet weak var signInButton: UIButton!
-    @IBOutlet weak var signOutButton: UIButton!
     @IBOutlet weak var cloudsView: UIImageView!
     @IBOutlet weak var cloudsViewLeading: NSLayoutConstraint!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("\nOn log-in screen\n")
         
         GIDSignIn.sharedInstance()?.delegate = self
         GIDSignIn.sharedInstance()?.uiDelegate = self    // Set the UI delegate of the GIDSignIn object
@@ -69,35 +69,17 @@ class LogInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDeleg
         button.layer.borderColor = WHITE.cgColor
         
     }
-    
-    @IBAction func signInPressed(_ sender: Any) {
-        
-        SVProgressHUD.show()
-        
-        Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
-            
-            if error != nil {
-                print(error!)
-            } else {
-                print("Log in successful!")
-                //self.performSegue(withIdentifier: "goToChat", sender: self)
-            }
-        }
-        
-        SVProgressHUD.dismiss()
 
-        
-    }
     
     // Present a sign-in with Google window
     @IBAction func googleSignIn(sender: AnyObject) {
+        
         GIDSignIn.sharedInstance().signIn()
+        
     }
     
+    
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        print("Google Sign In didSignInForUser")
-        
-        SVProgressHUD.show()
         
         if let error = error {
             print(error.localizedDescription)
@@ -105,17 +87,23 @@ class LogInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDeleg
         }
         guard let authentication = user.authentication else { return }
         let credential = GoogleAuthProvider.credential(withIDToken: (authentication.idToken)!, accessToken: (authentication.accessToken)!)
+                
         // When user is signed in
         Auth.auth().signInAndRetrieveData(with: credential, completion: { (user, error) in
             if let error = error {
-                print("Login error: \(error.localizedDescription)")
+                print("\nLogin error: \(error.localizedDescription)\n")
                 return
+            }
+            else {
+                print("\nLogged in successfully\n")
             }
         })
         
-        SVProgressHUD.dismiss()
+        print("\nShould segue\n")
+        performSegue(withIdentifier: "goToDashboard", sender: self)
 
     }
+    
     
     // Start Google OAuth2 Authentication
     func sign(_ signIn: GIDSignIn?, present viewController: UIViewController?) {
@@ -130,20 +118,6 @@ class LogInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDeleg
     func sign(_ signIn: GIDSignIn?, dismiss viewController: UIViewController?) {
         // Close OAuth2 authentication window
         dismiss(animated: true) {() -> Void in }
-    }
-    
-    @IBAction func signOutPressed(_ sender: Any) {
-        
-        print("Attempting to sign out")
-        
-        do {
-            try Auth.auth().signOut()
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
-        }
-        
-        print("Signed out")
-
     }
     
     
